942b979a3989c3d39e66bebc570194f0f06ea3bc
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-12-16 11:41:55 +0000

    testing database connection and fetching first data

--- inc/db-connect.inc.php
+++ inc/db-connect.inc.php
@@ -1,3 +1,15 @@
 <?php
- echo "Connecting database";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+    try{
+        $pdo = new PDO('mysql:host=localhost;dbname=names;charset=utf8mb4','names','86G5omzs(iP.0Uui',$options);
+    }catch(PDOException $e){
+        echo "A problem occured with the database connection...";
+        die();
+    }
+// test data
+$stmt = $pdo->prepare('SELECT * FROM `names`');    
+$stmt->execute();
+var_dump($stmt->fetch());
 ?>
