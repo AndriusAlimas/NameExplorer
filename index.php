207716bb65bb9a444554605d2d3e7d0a9870bf20
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-12-22 13:00:36 +0000

    get char from query and make sure its string type and only first character is grabded

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 require __DIR__ . "/inc/all.inc.php";
+
+$char = (string)($_GET['char'] ?? '');
+if(strlen($char) > 1){
+    $char = $char[0];
+}
 ?>
 
 <?php require __DIR__ . "/views/header.php" ?>
