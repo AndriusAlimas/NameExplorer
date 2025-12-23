df74f8eb3d99d86761d6aff9c264fc501e18cfac
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-12-23 10:03:42 +0000

    getting from database first letter distinct names when press any letter on front end page

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php 
 require __DIR__ . "/inc/all.inc.php";
 
-$char = (string)($_GET['char'] ?? '');
+$char = strtoupper((string)($_GET['char'] ?? ''));
 if(strlen($char) > 1){
     $char = $char[0];
 }
+$stmt = $pdo->prepare("SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :expr ORDER BY `name` ASC");
+$stmt->bindValue(":expr","{$char}%");
+$stmt->execute();
+
+$names = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <?php require __DIR__ . "/views/header.php" ?>
+<?php var_dump($names); ?>
 <?php require __DIR__ . '/views/footer.php' ?>
