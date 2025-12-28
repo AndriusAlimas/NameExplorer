3ef81903c52c39fc52a2806c538df4e830772722
Andrius Alimas <alimaswebdeveloper@gmail.com>
2025-12-28 11:48:09 +0000

    fetch names by initial

---
+++ inc/names.inc.php
@@ -0,0 +1,14 @@
+<?php
+function fetch_names_by_initial($char){
+    global $pdo;
+    $stmt = $pdo->prepare("SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :expr ORDER BY `name` ASC");
+    $stmt->bindValue(":expr","{$char}%");
+    $stmt->execute();
+
+    $names = [];
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach($results AS $result){
+        $names[] = $result['name'];
+    }
+    return $names;
+}
--- index.php
+++ index.php
@@ -5,13 +5,21 @@ $char = strtoupper((string)($_GET['char'] ?? ''));
 if(strlen($char) > 1){
     $char = $char[0];
 }
-$stmt = $pdo->prepare("SELECT DISTINCT `name` FROM `names` WHERE `name` LIKE :expr ORDER BY `name` ASC");
-$stmt->bindValue(":expr","{$char}%");
-$stmt->execute();
 
-$names = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$names = fetch_names_by_initial($char);
 ?>
 
 <?php require __DIR__ . "/views/header.php" ?>
-<?php var_dump($names); ?>
+
+<ul>
+    <?php foreach($names AS $name): ?>
+       
+        <li>
+            <a href="name.php?<?php echo http_build_query(['name' => $name]); ?>">
+                <?php echo e($name); ?>
+            </a>    
+       </li>
+    <?php endforeach; ?>
+</ul>
 <?php require __DIR__ . '/views/footer.php' ?>
